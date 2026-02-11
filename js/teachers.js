4760c0a765d824cd9511d772ad2e2a8d2ecd2e77
sannyyhra1604-sudo <sannyyhra1604@gmail.com>
2026-02-11 07:38:56 +0700

    Update teachers.js

--- js/teachers.js
+++ js/teachers.js
@@ -4,9 +4,9 @@ let filteredData = [];
 
 // FETCH & JOIN DATA
 Promise.all([
-  fetch('./data/teachers_rows.json').then(r=>r.json()),
+  fetch('./data/teacher_rows.json').then(r=>r.json()),
   fetch('./data/lesson_schedule_rows.json').then(r=>r.json()),
-  fetch('./data/lessons_rows.json').then(r=>r.json()),
+  fetch('./data/lesson_rows.json').then(r=>r.json()),
   fetch('./data/classes_rows.json').then(r=>r.json())
 ]).then(([teachers, schedules, lessons, classes]) => {
 
@@ -77,3 +77,4 @@ document.getElementById('nextBtn').onclick = () => {
     renderTable();
   }
 };
+
