7349fb323dd35090ee6d7205b30fdb72d3888384
sannyyhra1604-sudo <sannyyhra1604@gmail.com>
2026-02-11 08:13:40 +0700

    Update students.js

--- js/students.js
+++ js/students.js
@@ -5,7 +5,7 @@ let filteredData = [];
 
 // FETCH DATA
 Promise.all([
-  fetch('./data/students_rows.json').then(r => r.json()),
+  fetch('./data/student_rows.json').then(r => r.json()),
   fetch('./data/classes_rows.json').then(r => r.json())
 ]).then(([students, classes]) => {
 
